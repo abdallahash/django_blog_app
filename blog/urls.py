d678f16a7626fa92e2190aace30b000249c7e1ed
Abdallah <abdallahgxr@gmail.com>
2019-04-25 10:49:47 +0200

    Add new feature to create drafs and publish them.

--- blog/urls.py
+++ blog/urls.py
@@ -12,4 +12,9 @@ urlpatterns = [
 
     #selfengineered/post/2/edit/
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+
+    #selfengineered/drafts
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish')
 ]
--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,6 @@ def post_new(request):
             #False commit means saving without adding the stuff to the database.
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -37,10 +36,19 @@ def post_edit(request, pk):
         if form.is_valid():
             post= form.save(commit=False)   
             post.author = request.user 
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
         stuff_for_frontend = {'form': form}
     return render(request, 'blog/post_edit.html', stuff_for_frontend)
+
+def post_draft_list(request):
+    draft_posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+    stuff_for_frontend = {'draft_posts': draft_posts}
+    return render(request, 'blog/post_draft_list.html', stuff_for_frontend)
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
