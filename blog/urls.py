d5b3f47b08f46195b157408d685094911ac914af
Abdallah <abdallahgxr@gmail.com>
2019-04-25 22:20:04 +0200

    Add login functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from . import views 
+# from django.contrib.auth import views as auth_views 
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -16,5 +17,7 @@ urlpatterns = [
     #selfengineered/drafts
     path('drafts/', views.post_draft_list, name='post_draft_list'),
 
-    path('post/<int:pk>/publish/', views.post_publish, name='post_publish')
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
