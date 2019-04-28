c0bfbfeeea82db52feab7fbe7becff8e9b14e246
Abdallah <abdallahgxr@gmail.com>
2019-04-28 21:10:15 +0200

    Require login to post, remove, or approve comments

--- blog/views.py
+++ blog/views.py
@@ -58,6 +58,7 @@ def post_publish(request, pk):
     post.publish()
     return redirect('post_detail', pk=pk)
 
+@login_required 
 def comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
@@ -72,11 +73,13 @@ def comment_to_post(request, pk):
         form = CommentForm()
     return render(request, 'blog/comment_to_post.html', {'form': form})
 
+@login_required 
 def remove_comment(request, pk):
     comment = get_object_or_404(Comment, pk = pk)
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
 
+@login_required 
 def approve_comment(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
